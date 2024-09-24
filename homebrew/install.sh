e2186f89de11e5777cacc8c17fc7fd2d1edd2036
Pixel <scott.blatt.0b10@gmail.com>
2024-09-24 14:25:22 -0500

    defaults

--- homebrew/install.sh
+++ homebrew/install.sh
@@ -23,13 +23,16 @@ mkdir bin Applications
 if [[ "$HOMEBREW_ETC_REPO" == "NULL" ]]; then
   mkdir -p etc/homebrew
   cat <<EOF > etc/homebrew/brew.env
+\# Automatically generated settings
 HOMEBREW_CASK_OPTS=--appdir=$HOMEBREW_PREFIX/Applications
 HOMEBREW_BUNDLE_FILE_GLOBAL=$HOMEBREW_PREFIX/etc/homebrew/Brewfile
+HOMEBREW_NO_ENV_HINTS=1
 EOF
   cat <<EOF > etc/homebrew/Brewfile
 tap "homebrew/bundle"
 tap "homebrew/cask"
 tap "p1x3l101-10/personal"
+brew "p1x3l101-10/personal/brew-tools"
 EOF
 else
   git clone "$HOMEBREW_ETC_REPO" "$HOMEBREW_PREFIX/etc"
