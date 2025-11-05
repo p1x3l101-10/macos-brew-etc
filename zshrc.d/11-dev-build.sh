541de67598d298b2818cc566668c85f24a8da612
Pixel <scott.blatt.0b10@gmail.com>
2025-11-05 10:00:12 -0600

    update

--- zshrc.d/11-dev-build.sh
+++ zshrc.d/11-dev-build.sh
@@ -2,3 +2,9 @@ LIBRARY_PATH="$(brew --prefix)/lib"
 export LIBRARY_PATH
 export VCPKG_ROOT="$(brew --prefix)/Library/VCPkg"
 launchctl setenv VCPKG_ROOT "$VCPKG_ROOT"
+export CC="$(brew --prefix)/bin/clang"
+export CXX="$(brew --prefix)/bin/clang++"
+PATH="/private/var/tmp/.homebrew/opt/make/libexec/gnubin:$PATH"
+export SDKROOT=/Library/Developer/CommandLineTools/SDKs/MacOSX.sdk
+export CPM_SOURCE_CACHE="$HOME/.cache/cpm"
+export CMAKE_BUILD_PARALLEL_LEVEL=8
